c650fae65cb421f184e783c7340dfa4299efc084
Nigglatio <nigglatio@github.com>
2018-01-24 16:59:22 +0000

    Event and User datatables
    Done with the Datatables for User and Event logs

--- eventlogslist.php
+++ eventlogslist.php
@@ -18,48 +18,34 @@ $requestData= $_REQUEST;
 
 $columns = array(
 // datatable column index  => database column name
-	0 => 'eventlog_id',
-	1 => 'event_title',
-	2 => 'user_id',
-	3 => 'action',
-	4 => 'date'
+	0 => 'firstname',
+	1 => 'date'
 );
 
 // getting total number records without any search
-$sql = "select p.eventtitle,p.date_to_be_organized,r.regionname as region,p.town,u.firstname as fname, u.lastname as lname,p.pending_id FROM `pending` as p INNER JOIN region as r on r.region_id = p.region INNER JOIN users as u on u.userid = p.reporter";
-$query=mysqli_query($conn, $sql) or die("level1pendinglist.php: get information0");
+$sql = "select e.eventlog_id,e.event_title,u.firstname as fname,u.lastname as lname,e.action,e.date from eventlogs as e inner join users as u on u.userid = e.user_id";
+$query=mysqli_query($conn, $sql) or die("eventloglist.php: get information0");
 $totalData = mysqli_num_rows($query);
 $totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
 
-$sql = "select p.eventtitle,p.date_to_be_organized,r.regionname as region,p.town,u.firstname as fname, u.lastname as lname,p.pending_id FROM `pending` as p INNER JOIN region as r on r.region_id = p.region INNER JOIN users as u on u.userid = p.reporter";
+$sql = "select e.eventlog_id,e.event_title,u.firstname as fname,u.lastname as lname,e.action,e.date from eventlogs as e inner join users as u on u.userid = e.user_id";
 if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
-	$sql.=" Where ( eventtitle LIKE '".$requestData['search']['value']."%' )";
+	$sql.=" Where ( u.firstname LIKE '".$requestData['search']['value']."%'";
+	$sql.=" or u.lastname LIKE '".$requestData['search']['value']."%' )";
 }
-$query=mysqli_query($conn, $sql) or die("level1pendinglist.php: get information1");
+$query=mysqli_query($conn, $sql) or die("eventloglist.php: get information1");
 $totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result.
 $sql.=" ORDER BY ". $columns[$requestData['order'][0]['column']]." ".$requestData['order'][0]['dir']." LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
 /* $requestData['order'][0]['column'] contains column index, $requestData['order'][0]['dir'] contains order such as asc/desc  */
-$query=mysqli_query($conn, $sql) or die("level1pendinglist.php: get information2");
+$query=mysqli_query($conn, $sql) or die("eventloglist.php: get information2");
 
 $data = array();
 while( $row=mysqli_fetch_array($query) ) {  // preparing an array
 	$nestedData=array();
 
-	$nestedData[] = "<p style='text-align: left'>{$row['eventtitle']}</p>";
-	$enddate = date('jS F Y', strtotime($row['date_to_be_organized']));
+	$enddate = date('jS F Y', strtotime($row['date']));
+	$nestedData[] = $row['fname'].' '.$row['lname'].' '.$row['action'].' to '.$row['event_title'];
 	$nestedData[] = $enddate;
-	$nestedData[] = $row['region'];
-	$nestedData[] = $row['town'];
-	$nestedData[] = $row['fname'].' '.$row['lname'];
-
-   //  if( $row['is_approved'] == "0"){
-   //      $approveLabel = "<p style='color: red'>Not Approved</p>";
-   //    } else {
-   //      $approveLabel = "<p style='color: green'>Approved</p>";
-   //    }
-
-	$buttonshow = "<a onclick='level1viewer({$row['pending_id']})' class='btn btn-twitter btn-just-icon '><i class='material-icons'>assignment</i></a>";//chang function
-	$nestedData[] = $buttonshow;
 
 	$data[] = $nestedData;
 }
--- userlogslist.php
+++ userlogslist.php
@@ -1,5 +1,5 @@
 <?php
-//customized by Brian Martey 
+//customized by Brian Martey
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
 /** written by Brian Martey*/
@@ -13,60 +13,47 @@ $dbname = "ncaoutreach";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 
 /* Database connection end */
-// storing  request (ie, get/post) global array to a variable  
+// storing  request (ie, get/post) global array to a variable
 $requestData= $_REQUEST;
 
-$columns = array( 
-// datatable column index  => database column name	
-	0 => 'eventtitle',
-	1 => 'date_to_be_organized', 
-	2 => 'region',
-	3 => 'town',
-	4 => 'reporter',
-	5 => 'pending_id'
+$columns = array(
+// datatable column index  => database column name
+	0 => 'firstname',
+	1 => 'date'
 );
 
 // getting total number records without any search
-$sql = "select p.eventtitle,p.date_to_be_organized,r.regionname as region,p.town,u.firstname as fname, u.lastname as lname,p.pending_id FROM `pending` as p INNER JOIN region as r on r.region_id = p.region INNER JOIN users as u on u.userid = p.reporter";
-$query=mysqli_query($conn, $sql) or die("level1pendinglist.php: get information0");
+$sql = "select u.action, u.date, e.firstname as addedfname, e.lastname as addedlname, f.firstname as addedonfname, f.lastname as addedonlname from userlogs as u inner join users as e on e.userid = u.acted_id inner join users as f on f.userid = u.acted_on_id";
+$query=mysqli_query($conn, $sql) or die("eventloglist.php: get information0");
 $totalData = mysqli_num_rows($query);
 $totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
 
-$sql = "select p.eventtitle,p.date_to_be_organized,r.regionname as region,p.town,u.firstname as fname, u.lastname as lname,p.pending_id FROM `pending` as p INNER JOIN region as r on r.region_id = p.region INNER JOIN users as u on u.userid = p.reporter";
+$sql = "select u.action, u.date, e.firstname as addedfname, e.lastname as addedlname, f.firstname as addedonfname, f.lastname as addedonlname from userlogs as u inner join users as e on e.userid = u.acted_id inner join users as f on f.userid = u.acted_on_id";
 if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
-	$sql.=" Where ( eventtitle LIKE '".$requestData['search']['value']."%' )";
+	$sql.=" Where ( e.firstname LIKE '".$requestData['search']['value']."%'";
+	$sql.=" or e.lastname LIKE '".$requestData['search']['value']."%'";
+	$sql.=" or f.firstname LIKE '".$requestData['search']['value']."%'";
+	$sql.=" or f.lastname LIKE '".$requestData['search']['value']."%' )";
 }
-$query=mysqli_query($conn, $sql) or die("level1pendinglist.php: get information1");
-$totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
+$query=mysqli_query($conn, $sql) or die("eventloglist.php: get information1");
+$totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result.
 $sql.=" ORDER BY ". $columns[$requestData['order'][0]['column']]." ".$requestData['order'][0]['dir']." LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
-/* $requestData['order'][0]['column'] contains column index, $requestData['order'][0]['dir'] contains order such as asc/desc  */	
-$query=mysqli_query($conn, $sql) or die("level1pendinglist.php: get information2");
+/* $requestData['order'][0]['column'] contains column index, $requestData['order'][0]['dir'] contains order such as asc/desc  */
+$query=mysqli_query($conn, $sql) or die("eventloglist.php: get information2");
 
 $data = array();
 while( $row=mysqli_fetch_array($query) ) {  // preparing an array
 	$nestedData=array();
 
-	$nestedData[] = "<p style='text-align: left'>{$row['eventtitle']}</p>";
-	$enddate = date('jS F Y', strtotime($row['date_to_be_organized']));
+	$enddate = date('jS F Y', strtotime($row['date']));
+	$nestedData[] = $row['addedfname'].' '.$row['addedlname'].' '.$row['action'].' '.$row['addedonfname'].' '.$row['addedonlname'];
 	$nestedData[] = $enddate;
-	$nestedData[] = $row['region'];
-	$nestedData[] = $row['town'];
-	$nestedData[] = $row['fname'].' '.$row['lname'];
 
-   //  if( $row['is_approved'] == "0"){
-   //      $approveLabel = "<p style='color: red'>Not Approved</p>";
-   //    } else {
-   //      $approveLabel = "<p style='color: green'>Approved</p>";
-   //    }
-	
-	$buttonshow = "<a onclick='level1viewer({$row['pending_id']})' class='btn btn-twitter btn-just-icon '><i class='material-icons'>assignment</i></a>";//chang function
-	$nestedData[] = $buttonshow;
-	
 	$data[] = $nestedData;
 }
 
 $json_data = array(
-			"draw"            => intval( $requestData['draw'] ),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
+			"draw"            => intval( $requestData['draw'] ),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw.
 			"recordsTotal"    => intval( $totalData ),  // total number of records
 			"recordsFiltered" => intval( $totalFiltered ), // total number of records after searching, if there is no searching then totalFiltered = totalData
 			"data"            => $data   // total data array
